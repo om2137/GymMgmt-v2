46c5245c9319e1039794dd64aa16f429a6f94041
om2137 <omraut961@gmail.com>
2024-12-31 23:48:15 +0530

    client section, modal integration, card changes

--- components/ClientGrid.tsx
+++ components/ClientGrid.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { SearchBar } from "./commons/SearchBar";
 import Card from "./commons/Card";
+import { Modal } from "./commons/Modal";
 
 interface client{
     id: number;
@@ -19,6 +20,9 @@ interface props{
 
 export function ClientGrid({clients}:props){
     const [filter, setFilter] = useState('hidden');
+    const [visibility, setVisibility]= useState('hidden');
+    const [selectedClient, setSelectedClient] = useState(0);
+    const [activeCardId, setActiveCardId] = useState<number | null>(null);
     
     function handler(){
         if(filter ==='hidden'){
@@ -28,29 +32,49 @@ export function ClientGrid({clients}:props){
         }
    }
    return(
-        <div className="w-11/12 h-11/12 flex flex-wrap justify-center my-4 mx-auto bg-cyan-200 dark:bg-cyan-700 rounded-xl ">
+        <div className={`w-11/12 relative h-11/12 flex flex-wrap justify-center my-4 mx-auto bg-cyan-200 dark:bg-cyan-700 rounded-xl`} >
             <div className="flex justify-between items-center text-2xl text-white bg-cyan-800 dark:bg-cyan-900 font-bold px-6 py-4 w-full rounded-t-xl ">
                 <div className="my-2">Clients</div>
                 <div className={`${filter}`}><SearchBar/></div>
                 <button className="my-2" onClick={handler} >Filter</button>
             </div>
-            {/* <div>
-                <div className="m-2">
-                    <Card heading={`test`} description={`test`}/>
-                </div> 
-            </div> */}
-            <div  className=" w-full max-h-[36rem] overflow-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4">
+            
+            <div  
+                className= {`
+                    ${visibility === `flex`? 'pointer-events-none' : ''} 
+                    w-full max-h-[36rem] overflow-auto 
+                    grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 
+                    p-4`
+                }>
             {
-                clients.map((client: client)=>(
+                clients.map((client: client, index: number)=>(
                     
-                    <div key={client.id} className="relative group hover:z-50 focus:z-50 transition-all m-2">
-                        <Card heading={client.name} description={`age: ${client.age} address: ${client.address}}`}/>
+                    <div
+                        key={client.id}
+                        className={`
+                        relative group hover:z-10 focus:z-10 transition-all m-2 
+                        ${
+                            index >= clients.length - (clients.length % 4 || 4) 
+                            ? ' hover:bottom-10' 
+                            : ''
+                        }
+                        `}
+                    >
+                        <Card 
+                            id={client.id} 
+                            setVisibility={setVisibility} 
+                            client={client}
+                            setSelectedClient={setSelectedClient}
+                            isActive={activeCardId === client.id} 
+                            setActiveCardId={setActiveCardId}
+                        />
                     </div> 
                 ))
             }
-            
+            </div> 
+            <div className={`${visibility} absolute top-40`}>
+                <Modal visibility={visibility} setVisibility={setVisibility} client={clients[selectedClient-1]}/>
             </div>
-            
         </div>
     )
 }
--- components/commons/Card.tsx
+++ components/commons/Card.tsx
@@ -1,62 +1,85 @@
 
 import Image from "next/image";
 import DefaultImg from "/public/default.png"
+import { Dispatch, SetStateAction } from "react";
 
 interface Props {
-    heading: string;
-    description: string;
+    client: client;
+    setVisibility: Dispatch<SetStateAction<string>>;
+    setSelectedClient: Dispatch<SetStateAction<number>>;
+    setActiveCardId: Dispatch<SetStateAction<number | null>>;
+    isActive: boolean;
+    id: number;
 }
-
-export default function Card({ heading, description }: Props) {
+interface client {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+    gender: string;
+    address: string;
+}
+export default function Card({ client, setVisibility, setSelectedClient,setActiveCardId, isActive, id }: Props,) {
+    const handleToggle = () => {
+        setActiveCardId(isActive ? null : id); 
+    };
     
-    // const [visibility, setVisibility] = useState('hidden');
-   
-    // function handleVisiblity(){
-    //     console.log('click registered', visibility)
-
-    //     if(visibility === 'hidden' ){
-    //         setVisibility('flex') ;
-
-    //     }else{
-    //         setVisibility('hidden');
-    //     }
-    // }
-
     return (
-        <div className="hover:scale-110 group relative flex flex-col text-left text-black bg-cyan-600 rounded-xl shadow-md shadow-cyan-800">
-            <div className="relative">
-                <div className=" w-[260px] h-[166px] bg-cyan-600 rounded-xl">
+        <div 
+            className={`
+                ${isActive ? 'scale-110 z-50' : ''}
+                hover:scale-110 group relative flex flex-col text-left text-black bg-cyan-600 
+                rounded-xl shadow-lg shadow-cyan-200`
+            }
+        >
+            <div className="relative" onClick={handleToggle}>
+                <div className="w-3/4 h-[166px] bg-cyan-600 rounded-xl">
                     <Image
                         src={DefaultImg}
                         fill={true}
                         alt={"no image"}
                         className="rounded-lg"
                     />
-                </div>
-                <div className="absolute bottom-0 pl-4 pb-2 capitalize  opacity-100 group-hover:opacity-0">
-                    <p className="text-sm text-gray-700">{description}</p>
-                    <h2 className="text-2xl font-bold">{heading}</h2>
-                </div>
-            </div>
-            
-            <div className={`absolute w-full capitalize text-white flex flex-col items-left top-32 
-                 p-2 rounded-b-xl bg-cyan-800  opacity-0 group-hover:opacity-100`}>
-                <div className="flex justify-between p-1">
-                    <span>name</span>
-                    <span>age</span>
-                </div>
-                <div className="flex justify-between items-center p-1">
-                    <span className="text-sm">Contact</span>
-                    <span className="text-sm font-thin">
-                        address 
-                    </span>
-                    <button 
-                        onClick={()=>console.log("modal click")} 
-                        className="px-4 py-2 bg-white rounded-xl text-cyan-950"
-                    >
-                        mod
+                    <button className={`
+                        ${isActive ? "opacity-100" : "opacity-0"}
+                        bg-red-500 text-white text-sm rounded-lg p-2 absolute top-2 right-2 `
+                    }>
+                        X
                     </button>
                 </div>
+                <div className={`
+                    ${isActive ? 'opacity-0' : 'opacity-100'}
+                    absolute bottom-0 pl-4 pb-2 capitalize   group-hover:opacity-0`
+                }>
+                    <p className="text-sm text-gray-700">{client.address}</p>
+                    <h2 className="text-2xl font-bold">{client.name}</h2>
+                </div>
+            </div>
+
+            <div className={`absolute w-full capitalize text-white flex flex-col items-left top-32 
+                    p-2 rounded-b-xl bg-cyan-800  opacity-0 group-hover:opacity-100 shadow-lg shadow-cyan-200
+                    ${isActive ? "opacity-100" : "opacity-0"}
+                 `}
+            >
+                <div className="flex justify-between">
+                    <span className="font-semibold">{client.name}</span>
+                    <span>age:{client.age}</span>
+                </div>
+                <div className="flex justify-between items-center">
+                    <div className="text-sm w-20 text-wrap">
+                        80808888
+                    </div>
+                    <button
+                        onClick={() => { setVisibility('flex'); setSelectedClient(id); if(isActive){handleToggle()}  }}
+                        className="px-4 py-2 mx-1 my-2 bg-white rounded-xl text-sm text-cyan-950 shadow-sm shadow-cyan-300" 
+                    >
+                        Details
+                    </button>
+                </div>
+                <div className="flex justify-end items-end">
+                    
+                </div>
+                
             </div>
         </div>
     );
