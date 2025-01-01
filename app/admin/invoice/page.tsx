731eeb8f70e1a7df167cbcc7c6331459882471f5
om2137 <omraut961@gmail.com>
2025-01-01 23:50:32 +0530

    invoice structure defined, list component update with ux and modal

--- app/admin/invoice/page.tsx
+++ app/admin/invoice/page.tsx
@@ -9,22 +9,14 @@ async function getInvoices() {
       console.log(e);
     }
 }
-async function getClients() {
-  try {
-    const response = await axios.get("http://localhost:3000/api/clients")
-      return response.data;
-  }  catch(e) {
-    console.log(e);
-  }
-}
+
 export default async function Invoice(){
     
      const invoicesList =await getInvoices();
-     const clientList = await getClients();
 
     return(
         <div className="w-11/12 flex justify-center h-11/12 my-4 mx-auto">
-            <List listName={"Invoice List"} invoices={invoicesList} clients={clientList}/>
+            <List listName={"Invoice List"} invoices={invoicesList} />
         </div>
     )
 }
--- components/List.tsx
+++ components/List.tsx
@@ -2,16 +2,20 @@
 import { useState } from "react";
 import { FilterButton } from "./Buttons/Button";
 import filters from "@/Jsons/filter.json"
-import { Modal } from "./commons/Modal";
+import { InvoiceModal } from "./commons/Modal";
 
 interface Props{
     listName: string;
     invoices?: {
-        key: number;
-        invoiceId: string;
+        id: number;
         name: string;
-        amount: string;
-    }[],
+        PaidOn: string;
+        from: string; 
+        to: string; 
+        paymentType: string;
+        totalfee: number;
+    }[];
+    
     clients?: {
         id: number;
         name: string;
@@ -22,21 +26,34 @@ interface Props{
     }[]
 }
 interface invoice{
+    id: number;
+    name: string;
+    PaidOn: string;
+    from: string; 
+    to: string; 
+    paymentType: string;
+    totalfee: number;
+}
+interface filters{
     key: number;
     name: string;
     amount: string;
 }
 
-export function List({listName,invoices,clients}:Props){
+export function List({listName,invoices }:Props){
     const [fltr,setFltr] = useState('hidden');
     const [visibility, setVisibility]= useState('hidden');
+    const [selectedInvoice, setSelectedInvoice] = useState<number>(0);
+
+    console.log(invoices)
+
     function filterVisibility(){
         if(fltr === 'hidden'){setFltr('flex')}else{setFltr('hidden')}
     }
     return(
             <div className="h-4/5 w-[89%] mx-auto text-white dark:text">
                 <div className="relative h-full mx-12 mt-8 flex flex-col items-center capitalize bg-cyan-200 dark:bg-cyan-700 rounded-xl">
-                    <div className="w-full flex flex-col py-4 px-8 rounded-t-xl text-xl font-semibold bg-cyan-800 dark:bg-cyan-900">
+                    <div className={`${visibility==='flex'?'pointer-events-none':''} w-full flex flex-col py-4 px-8 rounded-t-xl text-xl font-semibold bg-cyan-800 dark:bg-cyan-900`}>
                         <div className="w-full flex justify-between px-4 py-2">
                             <div>{listName}</div>
                             <div>
@@ -45,33 +62,34 @@ export function List({listName,invoices,clients}:Props){
                         </div>
                         <div className={`absolute top-16 left-0 z=10 w-full ${fltr} bg-cyan-800 rounded-b-xl flex-col items-center px-4 py-2`}>
                             {
-                                filters?.map((invoice:invoice)=>(
-                                    <div key={invoice.key} className="py-2">{invoice.name}</div>
+                                filters?.map((filter:filters)=>(
+                                    <div key={filter.key} className="py-2">{filter.name}</div>
                                 ))
                             }
                         </div>
                     </div>
-                    <div className="w-full max-h-[32rem] min-h-40 overflow-auto overflow-x-hidden mx-4 pt-2" id="list">
+                    <div className={` ${visibility==='flex'?'pointer-events-none':''} w-full max-h-[32rem] min-h-40 overflow-auto overflow-x-hidden mx-4 pt-2" id="list`}>
                     
                     {
                         invoices?.map((invoice:invoice)=>(
-                            <div key={invoice.key} className="w-full h-20 flex items-center hover:scale-105" >
-                                <div 
+                            <div key={invoice.id} className="w-full h-20 flex items-center hover:scale-105" >
+                                <button 
                                     className="w-full flex justify-between  mx-8 my-auto p-4 bg-cyan-800 dark:bg-cyan-900 rounded-xl"
-                                    onClick={()=>{setVisibility('flex'); console.log("click") }}
+                                    onClick={()=>{setVisibility('flex'); console.log(invoice.totalfee); setSelectedInvoice(invoice.id) }}
                                 >
-                                    <div>{invoice.key}. {invoice.name}</div>
-                                    <div>{invoice.amount}</div>
-                                </div>
-                                
+                                    <div>{invoice.id}. {invoice.name}</div>
+                                    <div>{invoice.totalfee}</div>
+                                </button>
                             </div>
                         ))
                     }
-                    <div className={`${visibility} absolute top-32 left-60`}>
-                        <Modal visibility={visibility} setVisibility={setVisibility} client={clients?.[0]}/>
                     </div>
+                    <div className={`${visibility} absolute top-32`}>
+                        <InvoiceModal 
+                            visibility={visibility} setVisibility={setVisibility} 
+                            setSelectedInvoice={setSelectedInvoice} 
+                            invoice={invoices?.[selectedInvoice-1]}/>
                     </div>
-                    
                 </div>
             </div>
         )
