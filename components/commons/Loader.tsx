7ddd961431a9c79456ceaaa39cb8ae10c8cfb89b
om2137 <omraut961@gmail.com>
2025-01-04 23:44:53 +0530

    loader component

---
+++ components/commons/Loader.tsx
@@ -0,0 +1,9 @@
+export function Loader(){
+    return(
+        <div>
+            <div className="border-b-8 w-40 h-40 rounded-full animate-spin border-white bg-transparent m-2">
+                
+            </div>
+        </div>
+    )
+}
