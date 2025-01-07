1e84d35f7dd76362059881f385e36eab483fed4c
om2137 <omraut961@gmail.com>
2025-01-07 13:50:55 +0530

    get all clients api

--- app/api/clients/route.ts
+++ app/api/clients/route.ts
@@ -1,4 +1,10 @@
-import client from '@/Jsons/client.json'
+// import client from '@/Jsons/client.json'
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export async function GET(){
-    return Response.json(client)
+    // return Response.json(client)
+    const users = await prisma.members.findMany()
+    return Response.json(users)
 }
