56c1c79c77e1ca7e39426a3f03f1969f93219be1
om2137 <omraut961@gmail.com>
2025-01-15 23:51:45 +0530

    make invoice component in invoice main page

--- app/admin/invoice/page.tsx
+++ app/admin/invoice/page.tsx
@@ -1,4 +1,5 @@
 import { List } from "@/components/List";
+import { MakeInvoice } from "@/components/MakeInvoice";
 import axios from "axios";
 
 async function getInvoices() {
@@ -9,13 +10,22 @@ async function getInvoices() {
     console.log(e);
   }
 }
-
+async function getClients() {
+  try {
+    const response = await axios.get("http://localhost:3000/api/clients")
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
 export default async function Invoice() {
 
   const invoicesList = await getInvoices();
+  const clientList = await getClients();
   
   return (
-    <div className="w-11/12 flex justify-center h-11/12 my-4 mx-auto">
+    <div className="w-11/12 flex flex-col   h-11/12 my-4 mx-auto">
+      <MakeInvoice client={clientList}/>
       <List listName={"Invoice List"} invoices={invoicesList} />
     </div>
   )
