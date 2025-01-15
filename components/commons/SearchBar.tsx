9a02a348034af1dbdbd4dc3c6cfc3f16fa34c1af
om2137 <omraut961@gmail.com>
2025-01-15 23:51:11 +0530

    increased search bar reusability

--- components/commons/SearchBar.tsx
+++ components/commons/SearchBar.tsx
@@ -1,31 +1,33 @@
-import { useEffect, useMemo, useState } from "react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
-interface Props{
+interface Props {
     searchDisplay?: string;
+    list?: list[];
 }
+interface list {
+    id: number;
+    name?: string;
+    avatar?: string;
+    route?: string;
+}
+export function SearchBar({ searchDisplay, list }: Props) {
+    const [searchInput, setSearchInput] = useState('');
+    const [filteredList, setFilteredList] = useState<{ id: number; name?: string; avatar?: string; }[]>([]);
 
-export function SearchBar( { searchDisplay }: Props ){
-    const [searchInput, setSearchInput] =useState('');
-    const [filteredList, setFilteredList] = useState<{ key: number; name: string }[]>([]);
-    const menu = useMemo(()=>[
-            {key:1, name:"Profiles"},
-            {key:2, name:"admission"},
-            {key:3, name:"invoice"},
-            {key:4, name:"SignIn"},
-        ],[]);
-
-    useEffect(()=>{
+    console.log(list)
+    useEffect(() => {
         console.log("rerender");
-        if(searchInput.trim() !== ''){
-            const filtered = menu.filter((item) =>
-                item.name.toLowerCase().includes(searchInput.toLowerCase())
+        if (searchInput.trim() !== '') {
+            const filtered = list?.filter((item) =>
+                item.name?.toLowerCase().includes(searchInput.toLowerCase())
             );
-            setFilteredList(filtered);
-        } else{
+            setFilteredList(filtered || []);
+        } else {
             setFilteredList([])
         }
-    },[searchInput,menu,searchDisplay])
-    
+    }, [searchInput, searchDisplay, list])
+
     return (
         <>
             <div className={`flex ${searchDisplay} w-full capitalize justify-center items-center `}>
@@ -39,10 +41,10 @@ export function SearchBar( { searchDisplay }: Props ){
                         placeholder="search"
                         className="w-full px-4 py-2 capitalize text-white dark:text-cyan-950 bg-cyan-800 dark:bg-white border border-r-0 border-white dark:border-cyan-950 dark:focus:border-cyan-950 focus:outline-none rounded-l-xl"
                     />
-                    <div 
+                    <div
                         className=" px-4 py-2 capitalize bg-cyan-800 dark:bg-white text-white dark:text-cyan-950 border border-l-0 border-white dark:border-cyan-950 dark:focus:border-cyan-950 focus:outline-none rounded-r-xl"
                     >
-                        <button onClick={()=> {
+                        <button onClick={() => {
                             setFilteredList([]);
                             setSearchInput('');
                         }}>
@@ -51,19 +53,23 @@ export function SearchBar( { searchDisplay }: Props ){
                     </div>
                 </div>
 
-                </div>
-                <div className={`${searchDisplay} flex flex-col w-full  capitalize text-center font-medium font-mono text-2xl`}>
-                    {
-                        filteredList.length > 0 || filteredList === undefined ?
-                            filteredList.map((item)=>(
-                                <button key={item.key} className="py-4 capitalize">
-                                    {item.name}
-                                </button>
-                            )) : ( <span></span>
-                            
-                            )
-                    }
-                </div>
+            </div>
+            <div className={`${searchDisplay} flex flex-col w-full  capitalize text-center font-medium font-mono text-2xl`}>
+                {
+                    filteredList.length > 0 || filteredList === undefined ?
+                        filteredList.map((item) => (
+                            <button key={item.id} className="py-4 capitalize">
+                                {item.avatar? 
+                                    <div className=" w-6 h-6 mx-4"><Image src={item.avatar} width={6} height={6} alt="no image" /></div> 
+                                    : null
+                                } <div>{item.name}</div>
+                                
+                            </button>
+                        )) : (<span></span>
+
+                        )
+                }
+            </div>
         </>
     )
 }
