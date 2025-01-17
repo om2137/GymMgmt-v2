e4a187f7004337be203c3196c2532e6252afd6b2
om2137 <omraut961@gmail.com>
2025-01-17 23:47:41 +0530

    invoice successfully inserting to db with valid memberid

--- components/MakeInvoice.tsx
+++ components/MakeInvoice.tsx
@@ -1,6 +1,6 @@
 'use client'
-// import axios from "axios";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Button } from "./Buttons/Button";
 import { SearchBar } from "./commons/SearchBar";
 import { Dropdown } from "./InputComp/DropDown";
@@ -55,28 +55,29 @@ export function MakeInvoice(Props: props) {
     const [paymentType, setPaymentType] = useState('');
     const [duration, setDuration] = useState('');
     const [admissionFee, setAdmissionFee] = useState('0');
+    const [clientId, setClientId] = useState(0);
+
     const clientList = Props.client?.map((member)=>({
         id:member.id, 
         name: `${member.Firstname} ${member.Lastname}`, 
         avatar: 'https://res.cloudinary.com/dqpsoptzm/image/upload/v1736962825/V2/default_m9uvjp.png'
     }));
-    // async function insertInvoice() {
-    //     const response = await axios.post(`http://localhost:3000/api/invoices`, newInvoice);
-    //     alert(response);
-    //     console.log(response);
-    // }
-
-    function handleChange() {
-
+    useEffect(()=>{
         setNewInvoice({
             ...newInvoice,
-            member_id: 25,
+            member_id: clientId,
             facility: facility,
             fees: Number(admissionFee) + (Number(duration) * Number(facility)),
             admFee: Number(admissionFee),
             paymentType: paymentType
         })
-        console.log(newInvoice)
+    },[clientId, admissionFee, duration, facility, paymentType])
+
+    async function insertInvoice() {
+        const response = await axios.post(`http://localhost:3000/api/invoices`, newInvoice);
+        alert(response);
+        console.log(response);
+        // console.log(newInvoice)
     }
     function handlePaidDate(e: { target: { value: string | number | Date; }; }) {
         const dueDate = new Date(e.target.value);
@@ -97,37 +98,33 @@ export function MakeInvoice(Props: props) {
                 <div className="flex justify-center capitalize text-xl font-semibold m-4">
                     <div className="p-2">member: </div>
                     <div>
-                        <SearchBar list={clientList} />
-                    </div>
-
-                </div>
-                <div className="grid grid-cols-2">
-                    <div className="mx-6">
-                        <Dropdown list={facilityList} setValue={setFacility} name={"facility"} placeholder={"select facility"} />
-                    </div>
-                    <div className="mx-6">
-                        <Dropdown list={paymentTypeList} setValue={setPaymentType} name={"payment Type"} placeholder={"select PaymentType"} />
-                    </div>
-                    <div className="mx-6">
-                        <Dropdown list={durationList} setValue={setDuration} name={"duration"} placeholder={"select Duration"} />
-                    </div>
-
-                    <div className="mx-6">
-                        <Dropdown list={admissionFeeList} setValue={setAdmissionFee} name={"admission Fee"} placeholder={"select admission fee"} />
+                        <SearchBar list={clientList} setId={setClientId}/>
                     </div>
                 </div>
-                <div className="grid grid-cols-2 m-4">
-                    <div>
+                <div className="w-full flex flex-wrap justify-center p-4">
+                    <div className="mx-6 my-2">
+                        <Dropdown width='w-24 md:w-40 lg:w-48' list={facilityList} setValue={setFacility} name={"facility"} placeholder={"select facility"} />
+                    </div>
+                    <div className="mx-6 my-2">
+                        <Dropdown width='w-24 md:w-40 lg:w-48' list={paymentTypeList} setValue={setPaymentType} name={"payment Type"} placeholder={"select PaymentType"} />
+                    </div>
+                    <div className="mx-6 my-2">
+                        <Dropdown width='w-24 md:w-40 lg:w-48' list={durationList} setValue={setDuration} name={"duration"} placeholder={"select Duration"} />
+                    </div>
+                    <div className="mx-6 my-2">
+                        <Dropdown width='w-24 md:w-40 lg:w-48' list={admissionFeeList} setValue={setAdmissionFee} name={"admission Fee"} placeholder={"select admission fee"} />
+                    </div>
+                    <div className="flex justify-between w-40 md:w-60 lg:w-64 mx-6 my-2">
                         <label htmlFor="dob" className="p-2">Paid On:</label>
                         <input
                             type="date"
                             onChange={handlePaidDate}
-                            className="rounded bg-cyan-100 border-cyan-900 dark:bg-cyan-900 border dark:border-white p-2 focus:outline-none mx-2"
+                            className="w-28 rounded bg-cyan-100 border-cyan-900 dark:bg-cyan-900 border dark:border-white p-2 focus:outline-none mx-2"
                             name="dob"
                         />
                     </div>
-                    <div>
-                        <label htmlFor="start" className="p-2">plan start from:</label>
+                    <div className="flex justify-between w-40 md:w-60 lg:w-64 mx-6 my-2">
+                        <label htmlFor="start" className="p-2">start from:</label>
                         <input
                             type="date"
                             onChange={(e) => {
@@ -136,13 +133,13 @@ export function MakeInvoice(Props: props) {
                                     paidDate: new Date(e.target.value).toISOString(),
                                 })
                             }}
-                            className="rounded bg-cyan-100 border-cyan-900 dark:bg-cyan-900 border dark:border-white p-2 focus:outline-none mx-2"
+                            className="w-28 rounded bg-cyan-100 border-cyan-900 dark:bg-cyan-900 border dark:border-white p-2 focus:outline-none mx-2"
                             name="StartDate"
                         />
                     </div>
                 </div>
                 <div className="flex justify-end pr-10 ">
-                    <Button Name={"Insert"} properties="" onClick={handleChange} />
+                    <Button Name={"Insert"} properties="" onClick={()=>{insertInvoice();}} />
                 </div>
             </div>
         </div>
