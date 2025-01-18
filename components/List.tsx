4d3f4c5e390f48caf4171a9d46f6751e8a5bcb8b
om2137 <omraut961@gmail.com>
2025-01-19 00:01:41 +0530

    invoice details updated with member details

--- components/List.tsx
+++ components/List.tsx
@@ -8,14 +8,27 @@ interface Props {
     listName: string;
     invoices?: {
         id: number;
-        member_id: number;
+        member: 
+        {
+            id: number;
+            Firstname: string;
+            Lastname: string;
+            address: string;
+            contact: string;
+            dob: Date;
+            gender: string;
+            IsMarried: boolean;
+            admissionDate: string;
+            cardNumber: string;
+            balance: string;
+        };
         paidDate: string;
         paidOn: string;
         dueDate: string;
         facility: string;
         fees: number;
         admFee: number;
-        paymentType: string;
+        PaymentType: string;
     }[];
 
     clients?: {
@@ -29,14 +42,26 @@ interface Props {
 }
 interface invoice {
     id?: number;
-    member_id: number;
+    member: {
+        id: number;
+        Firstname: string;
+        Lastname: string;
+        address: string;
+        contact: string;
+        dob: Date;
+        gender: string;
+        IsMarried: boolean;
+        admissionDate: string;
+        cardNumber: string;
+        balance: string;
+    };
     paidDate: string;
     paidOn: string;
     dueDate: string;
     facility: string;
     fees: number;
     admFee: number;
-    paymentType: string;
+    PaymentType: string;
 }
 interface filters {
     key: number;
@@ -49,14 +74,12 @@ export function List({ listName, invoices }: Props) {
     const [visibility, setVisibility] = useState('hidden');
     const [selectedInvoice, setSelectedInvoice] = useState<number>(0);
 
-    console.log(invoices)
-
     function filterVisibility() {
         if (fltr === 'hidden') { setFltr('flex') } else { setFltr('hidden') }
     }
     return (
         <div className="h-4/5 w-[89%] mx-auto text-white dark:text">
-            <div className="relative h-full sm:mx-12 mt-8 flex flex-col items-center capitalize bg-cyan-200 dark:bg-cyan-700 rounded-xl">
+            <div className=" h-full sm:mx-12 mt-8 flex flex-col items-center capitalize bg-cyan-200 dark:bg-cyan-700 rounded-xl">
                 <div className={`${visibility === 'flex' ? 'pointer-events-none' : ''} w-full flex flex-col py-4 px-8 rounded-t-xl text-xl font-semibold bg-cyan-800 dark:bg-cyan-900`}>
                     <div className="w-full flex justify-between px-4 py-2">
                         <div>{listName}</div>
@@ -82,18 +105,19 @@ export function List({ listName, invoices }: Props) {
                                     className="w-full flex justify-between  mx-8 my-auto p-4 bg-cyan-800 dark:bg-cyan-900 rounded-xl"
                                     onClick={() => { setVisibility('flex'); console.log(); setSelectedInvoice(index) }}
                                 >
-                                    <div>{invoice.id}. {invoice.facility}</div>
+                                    <div>{invoice.id}. {invoice.member.id}</div>
                                     <div>{invoice.fees}</div>
                                 </button>
                             </div>
                         ))
                     }
                 </div>
-                <div className={`${visibility} absolute top-32`}>
+                <div className={`${visibility} absolute top-64`}>
                     <InvoiceModal
                         visibility={visibility} setVisibility={setVisibility}
                         setSelectedInvoice={setSelectedInvoice}
-                        invoice={invoices?.[selectedInvoice - 1]} />
+                        invoice={invoices?.[selectedInvoice]} 
+                    />
                 </div>
             </div>
         </div>
