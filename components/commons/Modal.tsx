c28901ea8701817e99732083ef05bfdc3a51505d
om2137 <omraut961@gmail.com>
2025-01-18 23:59:58 +0530

    invoice modal update

--- components/commons/Modal.tsx
+++ components/commons/Modal.tsx
@@ -2,6 +2,7 @@
 
 import { Dispatch, SetStateAction } from "react";
 import { Button } from "../Buttons/Button";
+import { formatDateToDDMMYYYY } from "@/utils/DateFormatter";
 
 interface props {
     visibility: string;
@@ -21,14 +22,26 @@ interface props {
     };
     invoice?: {
         id: number;
-        member_id: number;
+        member: {
+            id: number;
+            Firstname: string;
+            Lastname: string;
+            address: string;
+            contact: string;
+            dob: Date;
+            gender: string;
+            IsMarried: boolean;
+            admissionDate: string;
+            cardNumber: string;
+            balance: string;
+        };
         paidDate: string;
         paidOn: string;
         dueDate: string;
         facility: string;
         fees: number;
         admFee: number;
-        paymentType: string;
+        PaymentType: string;
     };
     setSelectedInvoice?: Dispatch<SetStateAction<number>>;
 }
@@ -55,18 +68,24 @@ export function Modal({ visibility, setVisibility, client }: props) {
 
 export function InvoiceModal({ visibility, setVisibility, invoice, setSelectedInvoice }: props) {
 
+    
+    console.log(invoice)
+
     return (
-        <div className={`${visibility} w-[32rem] h-[26rem]`} >
+        <div className={`${visibility} w-screen sm:w-[32rem] h-[26rem] p-4`} >
             <div className={`w-full h-full relative flex justify-center items-center bg-cyan-800
              rounded-xl shadow shadow-cyan-400`}>
                 <div className="flex flex-col p-2 m-2">
-                    <span className="text-4xl font-semibold capitalize">hello, {invoice?.id}<br /> your invoice id: {invoice?.id}</span>
-                    <span className="text-2xl">your invoice is of Rs.{invoice?.fees}</span>
-                    <span className="text-lg">payment type: {invoice?.paymentType} <br /> paidOn: {invoice?.paidOn}</span>
-                    <span className="text-lg">Subscription validity: {invoice?.paidDate} - {invoice?.dueDate}</span>
+                    <span className="text-4xl font-semibold capitalize">hello, {invoice?.member.Firstname} {invoice?.member.Lastname}</span>
+                    <span className="text-2xl font-semibold capitalize"> your invoice id: {invoice?.id} </span>
+                    <span className="text-xl">your invoice is of Rs.{invoice?.fees}</span>
+                    <span className="">payment type: {invoice?.PaymentType} <br /> paidOn: {formatDateToDDMMYYYY(invoice?.paidOn)}</span>
+                    <span className="">Subscription validity: <br />{formatDateToDDMMYYYY(invoice?.paidDate)} - {formatDateToDDMMYYYY(invoice?.dueDate)}</span>
+                    <span><Button Name="download" btnColor="green"/></span>
+                    
                 </div>
 
-                <div className="absolute right-2 top-2">
+                <div className="absolute right-2 top-2" >
                     <Button Name={"close"} onClick={() => { setVisibility('hidden'); setSelectedInvoice?.(0); }} />
                 </div>
 
