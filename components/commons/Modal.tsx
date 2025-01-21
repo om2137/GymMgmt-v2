af71b8c92dc8462c9dabe1d419272fd65763945c
om2137 <omraut961@gmail.com>
2025-01-21 23:59:28 +0530

    download invoice generated through react pdf renderer template in modal

--- components/commons/Modal.tsx
+++ components/commons/Modal.tsx
@@ -3,6 +3,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { Button } from "../Buttons/Button";
 import { formatDateToDDMMYYYY } from "@/utils/DateFormatter";
+import { DownloadInvoice } from "../invoice/PdfGenerator";
 
 interface props {
     visibility: string;
@@ -68,24 +69,26 @@ export function Modal({ visibility, setVisibility, client }: props) {
 
 export function InvoiceModal({ visibility, setVisibility, invoice, setSelectedInvoice }: props) {
 
-    
     console.log(invoice)
 
     return (
         <div className={`${visibility} w-screen sm:w-[32rem] h-[26rem] p-4`} >
-            <div className={`w-full h-full relative flex justify-center items-center bg-cyan-800
-             rounded-xl shadow shadow-cyan-400`}>
+            <div className={`w-full h-full relative flex justify-center items-center bg-cyan-800 rounded-xl shadow shadow-cyan-400`}>
                 <div className="flex flex-col p-2 m-2">
                     <span className="text-4xl font-semibold capitalize">hello, {invoice?.member.Firstname} {invoice?.member.Lastname}</span>
                     <span className="text-2xl font-semibold capitalize"> your invoice id: {invoice?.id} </span>
                     <span className="text-xl">your invoice is of Rs.{invoice?.fees}</span>
                     <span className="">payment type: {invoice?.PaymentType} <br /> paidOn: {formatDateToDDMMYYYY(invoice?.paidOn)}</span>
                     <span className="">Subscription validity: <br />{formatDateToDDMMYYYY(invoice?.paidDate)} - {formatDateToDDMMYYYY(invoice?.dueDate)}</span>
-                    <span><Button Name="download" btnColor="green"/></span>
+                    <span>
+                        {invoice && <DownloadInvoice invoice={invoice} />}
+                        {/* <Button Name="download" btnColor="green"/> */}
+                    </span>
                     
                 </div>
-
+                
                 <div className="absolute right-2 top-2" >
+                    
                     <Button Name={"close"} onClick={() => { setVisibility('hidden'); setSelectedInvoice?.(0); }} />
                 </div>
 
