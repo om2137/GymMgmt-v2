c442e01ccf174811c9b204c025b92ed5bd9cc3f5
om2137 <omraut961@gmail.com>
2025-01-21 23:59:19 +0530

    download invoice generated through react pdf renderer template in modal

--- components/commons/SearchBar.tsx
+++ components/commons/SearchBar.tsx
@@ -29,7 +29,7 @@ export function SearchBar({ searchDisplay, list, setId }: Props) {
     }, [searchInput, searchDisplay, list])
 
     return (
-        <>
+        <div className="relative ">
             <div className={`flex ${searchDisplay} w-full capitalize justify-center items-center `}>
                 <div className="w-full flex items-center font-mono sm:text-lg">
                     <input
@@ -54,7 +54,7 @@ export function SearchBar({ searchDisplay, list, setId }: Props) {
                 </div>
                         
             </div>
-            <div className={`${searchDisplay} flex flex-col w-full  capitalize text-center font-medium font-mono text-2xl`}>
+            <div className={`${searchDisplay} absolute flex flex-col w-full rounded-2xl capitalize text-center font-medium font-mono text-2xl`}>
                 {
                     filteredList.length > 0 || filteredList === undefined ?
                         filteredList.map((item) => (
@@ -63,10 +63,12 @@ export function SearchBar({ searchDisplay, list, setId }: Props) {
                                 setFilteredList([]);
                                 setSearchInput('');
                             }} 
-                            className="flex py-4 capitalize"
+                            className="flex  top-0 w-full py-4 capitalize bg-cyan-700 hover:bg-cyan-600"
                             >
                                 {item.avatar? 
-                                    <div className="relative rounded-2xl w-16 h-10 mx-4"><Image src={item.avatar} fill alt="no image" className="rounded-xl"/></div> 
+                                    <div className="relative w-16 h-10 mx-4">
+                                        <Image src={item.avatar} fill alt="no image" className="rounded-xl"/>
+                                    </div> 
                                     : null
                                 } <div>{item.name}</div>
                                 
@@ -76,6 +78,6 @@ export function SearchBar({ searchDisplay, list, setId }: Props) {
                         )
                 }
             </div>
-        </>
+        </div>
     )
 }
