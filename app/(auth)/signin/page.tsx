83b5542144b17ebc8cb76420be1d4cebb4841efc
om2137 <omraut961@gmail.com>
2025-01-23 23:58:00 +0530

    mesh gradient css in signin

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -2,19 +2,31 @@ import { Footer } from "@/components/commons/Footer";
 import { Header } from "@/components/commons/header";
 import { SignIn } from "@/components/auth/SignIn";
 
-export default function signin(){
+export default function signin() {
     return (
         <div className="flex flex-col h-screen text-amber-950 dark:text-white  no-scrollbar">
+            <MeshGradient />
             <div className="fixed top-0 left-0 w-full z-50">
                 <Header />
             </div>
 
             <div className=" my-auto">
-                <SignIn/>
+                <SignIn />
             </div>
             <div>
-                <Footer /> 
+                <Footer />
             </div>
         </div>
     );
 }
+
+const MeshGradient = () => {
+    return (
+        <div className="mesh-gradient-bg">
+            <div className="mesh-blur mesh-white"></div>
+            <div className="mesh-blur mesh-blue"></div>
+            <div className="mesh-blur mesh-pink"></div>
+            <div className="mesh-blur mesh-purple"></div>
+        </div>
+    );
+};
