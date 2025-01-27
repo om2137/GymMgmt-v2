72eada7dfa3a3a1c535a97ec15a9ddf51987d0dd
om2137 <omraut961@gmail.com>
2025-01-27 22:28:00 +0530

    signin button component combined with nextauth api

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Link from "next/link";
+import { signIn } from "next-auth/react";
 
 interface Props{
     route?: string;
@@ -12,29 +12,28 @@ interface Props{
     btnColor?: string;
 }
 
-export const SignInButton = function( {route, username, password,Name}: Props){
+export const SignInButton = function( { username, password,Name}: Props){
     
-    function checkUser(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+    function checkUser(){
         if(!username?.trim() && !password?.trim()){
-            e.preventDefault();
             alert(`missing fields`);
         }else if(username !== 'user' && password !== 'pass'){
 
         }
         else{
-            // alert(`${username} ${password}`);
+            alert(`${username} ${password}`);
+            signIn("credentials", { username: username, password: password })
         }
         
     }
 
     return(
         <div>
-            <Link 
-                href={route||""} 
+            <button 
                 onClick={checkUser} 
                 className=" hover:bg-white dark:hover:bg-cyan-950 hover:text-cyan-950 dark:hover:text-white rounded-xl py-2 px-4 capitalize">
                 {Name}
-            </Link>
+            </button>
         </div>
     );
 } 
@@ -45,6 +44,6 @@ export const FilterButton = function({onClick, Name}:Props){
 }
 export const Button = function({onClick, Name, properties, btnColor}:Props){    
     return(
-        <button onClick={onClick} className={`${properties} ${btnColor? btnColor : 'bg-red-500'} capitalize px-4 py-2 m-2  ${btnColor === 'bg-white'? 'text-cyan-950' : 'text-white'} rounded-lg`}>{Name}</button>
+        <button onClick={onClick} className={`${properties} ${btnColor? btnColor : 'bg-red-500'} capitalize px-4 py-2 m-2  ${btnColor === 'bg-white' || btnColor ==='bg-transparent'? 'text-cyan-950' : 'text-white'} rounded-lg`}>{Name}</button>
     )
 }
