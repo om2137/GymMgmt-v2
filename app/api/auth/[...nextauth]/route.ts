0cbbefedbd84334fa9d2c4d6d66d5847ac8c00c2
om2137 <omraut961@gmail.com>
2025-01-27 22:21:06 +0530

    initiated authorizing user and protecting routes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,37 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from 'next-auth/providers/credentials';
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
-        name: 'Credentials',
-        credentials: {
-          username: { label: 'email', type: 'text', placeholder: '' },
-          password: { label: 'password', type: 'password', placeholder: '' },
+      name: "Credentials",
+      credentials: {
+        username: { label: "User", type: "text", placeholder: "Username" },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "Password",
         },
-        async authorize() {
-            
-            return {
-                id: "user1"
-            };
-        },
-      })
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+        const user = credentials?.username ||"test";
+        return {
+          id: "user",
+          name: user,
+          email: 'test@gmail.com',
+          
+        };
+      },
+    }),
   ],
-})
+  callbacks: {
+    async redirect() {
+      return "/admin"; // Redirect to a custom page after login
+    },
+  },
+  
+  // pages:{signIn: '/auth/signin',}
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
