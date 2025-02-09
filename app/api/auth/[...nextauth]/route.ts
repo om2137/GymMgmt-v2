4bc6b5e242f65f42a17d3cf303bf0bd5ac64471f
om2137 <omraut961@gmail.com>
2025-02-09 23:50:30 +0530

    next auth checks user info from database

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+
+const prisma = new PrismaClient();
 
 const authOptions = {
   providers: [
@@ -14,26 +17,41 @@ const authOptions = {
         },
       },
       async authorize(credentials) {
-        console.log(credentials);
-        const user = credentials?.username ||"test";
+        console.log("checking data")
+        if (!credentials?.username || !credentials.password) {
+          alert("wrong info")
+          return null;
+        }
+        console.log(credentials.username);
+        const user = await prisma.admin.findFirst({
+          where:{
+            AdminName: credentials.username,
+            password: credentials.password
+          }
+        });
+        // const user = await prisma.admin.findMany();
+        console.log(user?.password);
+        if(!user){
+          // throw new Error("Invalid username or password");
+          return null;
+        }
+
         return {
           id: "user",
-          name: user,
-          email: 'test@gmail.com',
-          
+          name: credentials?.username,
+          email: "test@gmail.com",
         };
       },
     }),
   ],
   callbacks: {
     async redirect() {
-      return "/admin"; 
+      return `/admin`;
     },
   },
-  
-  // pages:{signIn: '/auth/signin',}
+  pages:{signIn: '/signin',}
 };
 
 const handler = NextAuth(authOptions);
 
-export {handler as POST, handler as GET}
+export { handler as POST, handler as GET };
