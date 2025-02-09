ae5f196080f4332ca7fa555a723b8d761eb5b014
om2137 <omraut961@gmail.com>
2025-02-09 23:52:13 +0530

    handeling signin error for signin inputbox

--- components/InputComp/Input.tsx
+++ components/InputComp/Input.tsx
@@ -1,41 +1,44 @@
 import { ChangeEvent, Dispatch, SetStateAction } from "react";
 
-interface Props{
-    name?:string;
-    placeholder?:string;
-    Id?:string;
+interface Props {
+    name?: string;
+    placeholder?: string;
+    error?: string;
+    Id?: string;
     setState?: Dispatch<SetStateAction<string>>;
-    changeHandler?: (e: ChangeEvent<any>) => void;
+    changeHandler?: (e: ChangeEvent<string | HTMLInputElement | undefined>) => void;
 }
-export function SignInInput({ placeholder, Id, setState }:Props){ 
-    return(
-        <input 
-            type="text" 
-            id={Id} 
-            onChange={(e)=>setState?.(e.target.value)}
-            className="text-center font-serif border-b-2 border-white dark:bg-white dark:border-cyan-950 bg-cyan-950 focus:border-b-2 focus:outline-none focus:placeholder:text-white px-8 py-2" 
+export function SignInInput({ placeholder, Id, setState, error }: Props) {
+
+    return (
+        <input
+            type="text"
+            id={Id}
+            onChange={(e) => setState?.(e.target.value)}
+            className={`text-center font-serif border-b-2 ${error ? 'border-red-500 placeholder:text-red-400' : 'border-white dark:border-cyan-950'} 
+                dark:bg-white bg-cyan-950 focus:border-b-2 focus:outline-none focus:placeholder:text-white px-8 py-2`}
             placeholder={placeholder}
         />
     )
 }
-export function FormInput({ placeholder, Id, setState }:Props){ 
-    return(
-        <input 
-            type="text" 
-            id={Id} 
-            onChange={(e)=>setState?.(e.target.value)}
-            className="text-center font-serif border-b-2 border-white dark:bg-white dark:border-cyan-950 bg-cyan-950 focus:border-b-2 focus:outline-none focus:placeholder:text-white px-8 py-2" 
+export function FormInput({ placeholder, Id, setState }: Props) {
+    return (
+        <input
+            type="text"
+            id={Id}
+            onChange={(e) => setState?.(e.target.value)}
+            className="text-center font-serif border-b-2 border-white dark:bg-white dark:border-cyan-950 bg-cyan-950 focus:border-b-2 focus:outline-none focus:placeholder:text-white px-8 py-2"
             placeholder={placeholder}
         />
     )
 }
-export function Input({ name,placeholder,changeHandler}:Props){ 
-    return(
-        <input 
-            type="text"  
+export function Input({ name, placeholder, changeHandler }: Props) {
+    return (
+        <input
+            type="text"
             name={name}
             onChange={changeHandler}
-            className="w-full text-start text-lg border-b-2 border-cyan-900 bg-cyan-100 dark:border-white dark:bg-cyan-900 focus:border-b-2 focus:outline-none focus:placeholder:text-white dark:focus:placeholder:text-cyan-900 p-1" 
+            className="w-full text-start text-lg border-b-2 border-cyan-900 bg-cyan-100 dark:border-white dark:bg-cyan-900 focus:border-b-2 focus:outline-none focus:placeholder:text-white dark:focus:placeholder:text-cyan-900 p-1"
             placeholder={placeholder}
         />
     )
