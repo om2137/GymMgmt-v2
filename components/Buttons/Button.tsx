7349256385c7cda16c6e4034a0a625906f0175da
om2137 <omraut961@gmail.com>
2025-02-17 23:00:55 +0530

    button component changes

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -3,6 +3,13 @@
 import { signIn } from "next-auth/react";
 
 interface Props{
+    onClick?: () => void;
+    Name: string;
+    properties?: string;
+    btnColor?: string;
+    size?: string;
+}
+interface SigninProps{
     route?: string;
     username?: string;
     password?: string;
@@ -12,8 +19,7 @@ interface Props{
     btnColor?: string;
 }
 
-export const SignInButton = function( { username, password,Name}: Props){
-    
+export const SignInButton = function( { username, password,Name}: SigninProps){
     function checkUser(){
         if(!username?.trim() && !password?.trim()){
             alert(`missing fields`);
@@ -22,9 +28,7 @@ export const SignInButton = function( { username, password,Name}: Props){
             alert(`${username} ${password}`);
             signIn("credentials", { username: username, password: password })
         }
-        
     }
-
     return(
         <div>
             <button 
@@ -40,8 +44,16 @@ export const FilterButton = function({onClick, Name}:Props){
         <button onClick={onClick} className="capitalize text-white">{Name}</button>
     )
 }
-export const Button = function({onClick, Name, properties, btnColor}:Props){    
+export const Button = function({onClick, Name, properties, btnColor, size}:Props){    
     return(
-        <button onClick={onClick} className={`${properties} ${btnColor? btnColor : 'bg-red-500'} capitalize px-4 py-2 m-2  ${btnColor === 'bg-white' || btnColor ==='bg-transparent'? 'text-cyan-950' : 'text-white'} rounded-lg`}>{Name}</button>
+        <button 
+            onClick={onClick} 
+            className={`
+                ${properties} ${btnColor? btnColor : 'bg-red-500'} capitalize 
+                ${size ? size :'px-8 py-2 m-2'}
+                ${btnColor === 'bg-white' || btnColor ==='bg-transparent'? 'text-cyan-950' : 'text-white'} rounded-lg`}
+            >
+                {Name}
+        </button>
     )
 }
