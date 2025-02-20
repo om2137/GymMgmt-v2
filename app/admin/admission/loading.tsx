f916c724808b506b76e13f206a8476c603c7f6c5
om2137 <omraut961@gmail.com>
2025-02-20 23:10:57 +0530

    admission form loader

---
+++ app/admin/admission/loading.tsx
@@ -0,0 +1,9 @@
+import Loader from "@/components/commons/Loader";
+
+export default function Loading(){
+    return(
+        <div className="h-full flex justify-center items-center">
+            <Loader />
+        </div>
+    ) 
+}
