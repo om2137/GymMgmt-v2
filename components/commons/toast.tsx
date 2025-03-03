147cb4f163bea97620943a4c2aa8cc8b9e91542c
om2137 <omraut961@gmail.com>
2025-03-03 22:35:44 +0530

    sonner component initiated

--- components/commons/toast.tsx
+++ components/commons/toast.tsx
@@ -1,7 +1,23 @@
-export function Toast(){
-    return (
-        <div>
-            <h1>Toast</h1>
-        </div>
-    )
+"use client"
+
+import { toast } from "sonner"
+
+import { Button } from "@/components/Buttons/Button"
+
+export function SonnerDemo() {
+  return (
+    <Button
+    //   variant="outline"
+        Name="Toast"
+      onClick={() =>
+        toast("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        })
+      }
+    />
+  )
 }
