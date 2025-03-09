89781522717349d137d9ac875a2b923b69f2b778
om2137 <omraut961@gmail.com>
2025-03-09 23:54:49 +0530

    default sonner component

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -11,16 +11,21 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group"
+      className="toaster group bg-cyan-800"
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            `group toast 
+              group-[.toaster]:bg-cyan-900 
+              group-[.toaster]:font-bold 
+              group-[.toaster]:text-foreground 
+              group-[.toaster]:border-border 
+              group-[.toaster]:shadow-lg`,
           description: "group-[.toast]:text-muted-foreground",
-          actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-          cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+          // actionButton:
+          //   "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          // cancelButton:
+          //   "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
       }}
       {...props}
